0403a57789514cc0127da3c214b58f4ecd67e6a1
frank.xu <frank.xu@touch-spring.com>
2016-05-23 16:00:34 +0800

    Initial Commit

---
+++ ChatBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatBar
+//
+//  Created by Xuzixiang on 16/5/23.
+//  Copyright © 2016年 TouchSpring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatBar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ChatBar
+//
+//  Created by Xuzixiang on 16/5/23.
+//  Copyright © 2016年 TouchSpring. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
